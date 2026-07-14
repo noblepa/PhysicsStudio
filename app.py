be0787f655c9530bedf83fcdd81fe88a235f446f
noblepa <noblepa@gmail.com>
2026-07-14 11:56:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+import streamlit as st
 
+st.set_page_config(
+    page_title="Physics Studio",
+    layout="wide"
+)
+
+st.title("Interactive Physics Studio")
+
+st.write("Welcome to the future home of interactive physics simulations!")
